50b6739d9622849d9c1f1210f93779abedf40c83
David GABISON <david.gabison@outlook.com>
2023-02-05 20:13:01 +0100

    refactor: split as method

--- main.ts
+++ main.ts
@@ -4,6 +4,8 @@ import type { Editor, MarkdownView } from 'obsidian';
 
 import { isNeedleAtIndex } from './util';
 
+const linkRe = /\[\[(?<target>[^[|]*)\|(?<alias>[^\]]*)\]\]/u;
+
 export default class NoteAliases extends Plugin {
   public onload (): void {
     this.addCommand({
@@ -11,8 +13,6 @@ export default class NoteAliases extends Plugin {
       name: 'Save alias of the link under cursor in the target note frontmatter',
 
       editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
-        const linkRe = /\[\[(?<target>[^[|]*)\|(?<alias>[^\]]*)\]\]/u;
-
         const position = editor.getCursor();
         const line = editor.getLine(position.line);
         const links = line.match(new RegExp(linkRe, 'gu')) ?? [];
@@ -21,37 +21,39 @@ export default class NoteAliases extends Plugin {
         if (!link) return false;
         if (checking) return true;
 
-        const { alias, target } = linkRe.exec(link)?.groups ?? {};
-        const fromPath = view.file.path;
-
-        (async (): Promise<void> => {
-          let targetFile = this.app.metadataCache.getFirstLinkpathDest(target, fromPath);
-          if (!targetFile) {
-            console.info('target file not exists', { checking, editor, view });
-            const targetPath = `${this.app.fileManager.getNewFileParent(fromPath).path}/${target}.md`;
-            targetFile = await this.app.vault.create(targetPath, '');
-            console.info(targetFile);
-          }
-          if (targetFile.extension !== 'md') return;
-          if (typeof this.app.fileManager.processFrontMatter === 'function') {
-            await this.app.fileManager.processFrontMatter(
-              targetFile,
-              (metadata: { aliases?: unknown }) => {
-                const aliases = parseFrontMatterAliases(metadata) ?? [];
-                const exists = aliases.some(item => item.toLowerCase() === alias.toLowerCase());
-
-                if (exists) return;
-
-                metadata.aliases = [...aliases, alias];
-              }
-            );
-            return;
-          }
-
-          );
-        })().catch(error => { console.error(error); });
+        this.saveAlias(editor, view, link).catch(error => { console.info(error); });
         return true;
       },
     });
+  }
+
+  private async saveAlias (editor: Editor, view: MarkdownView, link: string): Promise<void> {
+    const { alias, target } = linkRe.exec(link)?.groups ?? {};
+
+    const fromPath = view.file.path;
+    let targetFile = this.app.metadataCache.getFirstLinkpathDest(target, fromPath);
+
+    if (!targetFile) {
+      console.info('target file not exists', { editor, link, view });
+      const targetPath = `${this.app.fileManager.getNewFileParent(fromPath).path}/${target}.md`;
+      targetFile = await this.app.vault.create(targetPath, '');
+      console.info(targetFile);
+    }
+
+    if (targetFile.extension !== 'md') return;
+
+    if (typeof this.app.fileManager.processFrontMatter === 'function') {
+      await this.app.fileManager.processFrontMatter(
+        targetFile,
+        (metadata: { aliases?: unknown }) => {
+          const aliases = parseFrontMatterAliases(metadata) ?? [];
+          const exists = aliases.some(item => item.toLowerCase() === alias.toLowerCase());
+
+          if (exists) return;
+
+          metadata.aliases = [...aliases, alias];
+        }
+      );
+    }
   }
 }
